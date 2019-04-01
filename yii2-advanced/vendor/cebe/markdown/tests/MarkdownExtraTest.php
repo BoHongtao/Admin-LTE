28d0d0933d679416c2b8ce6d6b9cc3b67d4aac4d
boht@chocwell.com <BoHongtao@yeah.net>
2019-04-01 14:12:33 +0800

    yii-vendor

---
+++ yii2-advanced/vendor/cebe/markdown/tests/MarkdownExtraTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace cebe\markdown\tests;
+
+use cebe\markdown\MarkdownExtra;
+
+/**
+ *
+ * @author Carsten Brandt <mail@cebe.cc>
+ *         @group extra
+ */
+class MarkdownExtraTest extends BaseMarkdownTest {
+	public function createMarkdown() {
+		return new MarkdownExtra ();
+	}
+	public function getDataPaths() {
+		return [ 
+				'markdown-data' => __DIR__ . '/markdown-data',
+				'extra-data' => __DIR__ . '/extra-data' 
+		];
+	}
+}
