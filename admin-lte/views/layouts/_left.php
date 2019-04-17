a2f41efad77e805585336493cfe4b7817a8f573e
boht@chocwell.com <BoHongtao@yeah.net>
2019-04-17 08:49:22 +0800

    左侧菜单bug

--- admin-lte/views/layouts/_left.php
+++ admin-lte/views/layouts/_left.php
@@ -10,6 +10,8 @@ $cations = $controller . '/' . $action;
 <?php
 //菜单表中的权限
 $menus = login();
+//echo "<pre>";
+//print_r($menus);die;
 ?>
 <aside class="main-sidebar">
     <section class="sidebar">
@@ -18,30 +20,26 @@ $menus = login();
             <?php foreach ($menus as $value) : ?>
                 <?php if ($value['display'] == 2): ?>
                     <?php $result = explode(',', $value['attr']); ?>
-                    <li <?php if (in_array($controller, $result)): ?> class="active treeview menu-open" <?php endif; ?>>
-                        <a href="<?= Url::toRoute([$value['route']]) ?>" class="dropdown-toggle">
+                    <li <?php if (in_array($controller, $result)): ?>class="active treeview menu-open" <?php else: ?> class="treeview" <?php endif; ?>>
+                        <a href="#" class="dropdown-toggle">
                             <i class="fa fa-dashboard"></i>
                             <span class="menu-text"> <?php echo $value['name'] ?> </span>
-                            <?php if ('tourism/account_info' != $value['act']): ?>
-                                <span class="pull-right-container">
-                                    <i class="fa fa-angle-left pull-right"></i>
-                                </span>
-                            <?php endif; ?>
                         </a>
                         <?php if (isset($value['_child'])): ?>
                             <ul class="treeview-menu">
                                 <?php foreach ($value['_child'] as $val) : ?>
                                     <?php if ($val['display'] == 2): ?>
                                         <li <?php if ($cations == $val['route']): ?>class="active"<?php endif; ?>>
-                                            <a href="<?= Url::toRoute([$val['route']]) ?>"><i class="fa fa-circle-o"></i><?php echo $val['name'] ?></a>
+                                            <a href="<?= Url::toRoute([$val['route']]) ?>"><?php echo $val['name'] ?></a>
                                         </li>
                                     <?php endif; ?>
                                 <?php endforeach; ?>
                             </ul>
                         <?php endif; ?>
                     </li>
-                <?php endif;?>
-            <?php endforeach;?>
+                <?php endif; ?>
+            <?php endforeach; ?>
+
         </ul>
 
     </section>
