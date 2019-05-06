586b8713d22f60b00575e28b58b145bdc9c3a435
boht@chocwell.com <BoHongtao@yeah.net>
2019-05-06 17:43:04 +0800

    解决某些情况下菜单闪烁的bug

--- admin-lte/views/layouts/_header.php
+++ admin-lte/views/layouts/_header.php
@@ -9,10 +9,6 @@
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
-        <!-- Sidebar toggle button-->
-        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-            <span class="sr-only">Toggle navigation</span>
-        </a>
 
         <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
