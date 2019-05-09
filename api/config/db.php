365f3466516089edfe94975899e4a8128094bd9e
boht@chocwell.com <BoHongtao@yeah.net>
2019-05-09 16:47:09 +0800

    api

---
+++ api/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=base',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
