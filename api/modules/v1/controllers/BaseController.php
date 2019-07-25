6fcc6866994039a9cc9afe7901b78c1a25fbd0e4
boht@chocwell.com <BoHongtao@yeah.net>
2019-07-25 09:40:31 +0800

    跨域处理

--- api/modules/v1/controllers/BaseController.php
+++ api/modules/v1/controllers/BaseController.php
@@ -58,6 +58,21 @@ class BaseController extends Controller {
         //添加CORS过滤器
         $behaviors['corsFilter']=[
             'class'	=>\yii\filters\Cors::className(),
+            'cors' => [
+                // restrict access to
+                'Origin' => ['http://www.myserver.com', 'https://www.myserver.com'],
+                // Allow only POST and PUT methods
+                'Access-Control-Request-Method' => ['POST', 'PUT'],
+                // Allow only headers 'X-Wsse'
+                'Access-Control-Request-Headers' => ['X-Wsse'],
+                // Allow credentials (cookies, authorization headers, etc.) to be exposed to the browser
+                'Access-Control-Allow-Credentials' => true,
+                // Allow OPTIONS caching
+                'Access-Control-Max-Age' => 3600,
+                // Allow the X-Pagination-Current-Page header to be exposed to the browser.
+                'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
+            ],
+
         ];
         return $behaviors;
     }
